d9173149b1de50ae91e9a92d1d7b561f303be2cf
Helium <masters2020@gmail.com>
2016-01-07 23:09:59 -0500

    Update bitch.py

--- bitch.py
+++ bitch.py
@@ -1,4 +1,10 @@
 print("Yanika's first commit")
-print("Enter ur name")
-name = input()
-print("FUck u {}".format(name))
+'''Obsolete
+  print("Enter ur name")
+  name = input()
+'''
+name = input("Enter your name")
+print("Fuck u {}".format(name))
+if name == "Luke":
+  print("Easy nuh yute...bill dawg")
+  
