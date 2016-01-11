01b3225803b931903f5e2b6511e362ec1f40ece6
chenshuiluke <chenshuiluke@gmail.com>
2016-01-11 16:47:45 -0500

    Made automatic backup

--- automation.py
+++ automation.py
@@ -1,5 +1,42 @@
+from backup import Backup
+import json
+import shutil
+def createBackup(backups):
+	print(backups)
+	backup = Backup()
+	backup.getUserInput()
+	backups['backups'].append(json.loads(backup.returnJson()))
+	with open("presets.json", "w") as jsonWriter:
+		jsonWriter.write(json.dumps(backups, sort_keys = True, indent = 4))
+def getBackups():
+	backups = None
+	try:
+		with open("presets.json", "r") as jsonFile:
+			backups = json.load(jsonFile)
+	except:
+		backups = { "backups" : []}
+	return backups
+
+def runBackups(backups):
+	for backup in backups['backups']:
+		print("copying from {} to {}".format(backup['origin'], backup['destination']))
+		try:
+			shutil.copytree(backup['origin'], backup['destination'] + "/" + backup['origin'])
+		except (shutil.Error, FileExistsError) as err:
+			if type(err) == "shutil.Error":
+				errors = err[0]
+				for error in errors:
+					src, dst, why = error
+					print("Error copying {} : {}", src, why)
+		print("done copying")
+
 def main():
 	choice = None
-	while choice == None and choice != "3":
+	backups = getBackups()
+	while choice == None or choice != "3":
 		choice = input("1. Define new backup\n2. Run backups\n3. Exit\n")
+		if choice == "1":
+			createBackup(backups)
+		if choice == "2":
+			runBackups(backups)
 if(__name__ == "__main__"): main()
--- backup.py
+++ backup.py
@@ -1,8 +1,27 @@
+import json
+import os
 class Backup:
 	def __init__(self):
-		self.origin = None
-		self.destination = None
-	def setDestination(self, newDestination : string):
+		self.name = ""
+		self.origin = ""
+		self.destination = ""
+	def setDestination(self, newDestination : str):
 		self.destination = newDestination
-	def setOrigin(self, newOrigin : string):
+	def setOrigin(self, newOrigin : str):
 		self.origin = newOrigin
+
+	def getUserInput(self):
+			self.name = input("Please enter the name of the backup.")
+			while not os.path.isdir(self.origin):
+				self.origin = input("Please enter the name of the directory.")
+			while not os.path.isdir(self.destination) and self.destination != self.origin:
+				self.destination = input("Please enter the destination of the backup.")
+
+	def initFromJson(self, json):
+		self.name = json['name']
+		self.origin = json['origin']
+		self.destination = json['destination']
+
+	def returnJson(self):
+		print(json.dumps(self.__dict__))
+		return json.dumps(self.__dict__)
